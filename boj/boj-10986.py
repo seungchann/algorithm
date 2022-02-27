b57e3ced0a38c0cd9d9de828059181b859f0ee12
SChan-Kim <instufld@gmail.com>
2022-02-27 23:52:45 +0900

    feat: boj-2916, 10986, 1389.py

---
+++ boj/boj-10986.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+v = list(map(int, input().split()))
+
+pSum, cnt = 0, [0] * m
+for i in range(n):
+    pSum = (pSum + v[i]) % m
+    cnt[pSum] += 1
+
+ans = 0
+for i in range(m):
+    if i == 0:
+        ans += cnt[i] * (cnt[i] + 1) >> 1
+    else:
+        ans += cnt[i] * (cnt[i] - 1) >> 1
+print(ans)
