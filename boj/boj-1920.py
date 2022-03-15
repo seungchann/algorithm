162d548507388b2e089e4d78ae9f7725e62f21c3
SChan-Kim <instufld@gmail.com>
2022-03-16 00:02:39 +0900

    feat: bs part1

---
+++ boj/boj-1920.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+array = set(map(int, input().split()))
+
+m = int(input())
+x = list(map(int, input().split()))
+
+for i in x:
+    if i in array:
+        print(1)
+    else:
+        print(0)
