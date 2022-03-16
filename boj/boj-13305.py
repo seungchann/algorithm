dbbddba6fdc1400648dbad46b1b07edd3f5f99a7
SChan-Kim <instufld@gmail.com>
2022-03-16 23:36:49 +0900

    fix: boj-13305.py

--- boj/boj-13305.py
+++ boj/boj-13305.py
@@ -10,7 +10,9 @@ current_price = prices[0]
 current_distance = 0
 for idx in range(len(prices)-1):
     current_distance += distances[idx]
-    if (prices[idx] >= prices[idx+1]) or idx == len(prices)-1:
+    if (current_price >= prices[idx+1]) or idx == len(prices)-2:
+        print("distance: ", distances[idx])
+        print("current_price: ", current_price)
         result += current_price * current_distance
         current_price = prices[idx+1]
         current_distance = 0
