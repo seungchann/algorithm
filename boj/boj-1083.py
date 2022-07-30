bdba48aaec489e8bee4a554dc69a102161bec571
SChan-Kim <instufld@gmail.com>
2022-07-30 17:14:54 +0900

    feat: boj-1083, 1062.py

--- boj/boj-1083.py
+++ boj/boj-1083.py
@@ -1,17 +1,24 @@
 import sys
 input = sys.stdin.readline
 
-def bubble_sort(arr, cnt):
-    cur = 0
-    for i in range(len(arr)-1 , 0, -1):
-        for j in range(i):
-            if j < cnt-cur-1:
-                continue
-            if arr[j] < arr[j+1]:
-                arr[j], arr[j+1] = arr[j+1], arr[j]
-                cur += 1
-                if cur == cnt:
-                    return arr
+def solve(n, arr, cnt):
+    for i in range(n-1):
+        if cnt == 0:
+            break
+        
+        max_val, idx = arr[i], i
+        
+        for j in range(i+1, min(n, i+1+cnt)):
+            if max_val < arr[j]:
+                max_val = arr[j]
+                idx = j
+        
+        cnt -= (idx - i)
+    
+        for j in range(idx, i, -1):
+            arr[j] = arr[j-1]
+        
+        arr[i] = max_val
     
     return arr
 
@@ -19,4 +26,4 @@ n = int(input())
 array = list(map(int, input().split()))
 s = int(input())
 
-print(' '.join(map(str, bubble_sort(array, s))))
+print(*solve(n, array, s))
